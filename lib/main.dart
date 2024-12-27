1ac33baedb7d0c7e548d354832f9985cc0adee64
Sojib <sojib.cse3729@gmail.com>
2024-12-28 00:11:45 +0600

    warfaw

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,32 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:login_registration_userinfo_bloc_firebase/views/loginScreen.dart';
-import 'package:login_registration_userinfo_bloc_firebase/views/userInfo.dart';
-
-import 'bloc/login/login_bloc.dart';
-import 'bloc/registration/registration_bloc.dart';
-import 'firebase_options.dart';
+import 'package:login_registration_userinfo_bloc_firebase/bloc/app_bloc.dart';
+import 'package:login_registration_userinfo_bloc_firebase/controllers/home_controller.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final GetStorage storage = GetStorage();
-    final bool isLoggedIn = storage.read('isLoggedIn') ?? false;
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => RegistrationBloc(),
-        ),
-        BlocProvider(
-          create: (context) => SignInBloc(),
-        ),
-      ],
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarIconBrightness: Brightness.dark));
+    return BlocProvider(
+      create: (context) => AppBloc(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-          useMaterial3: true,
-        ),
-        home: isLoggedIn ?  UserInfoScreen() :  LoginScreen(),
-
-        //home: AuthScreen(),
-
-        //home: RegistrationScreen(),
-
-        // home: StreamBuilder<User?>(
-        //   stream: FirebaseAuth.instance.authStateChanges(),
-        //   builder: (context, snapshot) {
-        //     if (snapshot.connectionState == ConnectionState.waiting) {
-        //       return const Center(
-        //         child: CircularProgressIndicator(),
-        //       );
-        //     }
-        //     if (snapshot.hasData) {
-        //       return const UserInfoScreen();
-        //     } else {
-        //       return const LoginScreen();
-        //     }
-        //   },
-        // ),
-      ),
+          theme: ThemeData.light(),
+          home: ScreenController()),
     );
   }
 }
